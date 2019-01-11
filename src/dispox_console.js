b64ef7168a2fc8c6f33a7a7f8deaab09b7029bbc
Sanix Darker <saadjioa@gmail.com>
2019-01-11 09:08:39 +0100

    ScrollBottom feature + rmove unnecessary dependencies + little updates

--- src/dispox_console.js
+++ src/dispox_console.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './dispox_console.scss';
 
+// import axios from 'axios';
 
 class dispox_console extends Component {
 
@@ -23,8 +24,28 @@ class dispox_console extends Component {
     componentDidMount(){
 
     }
-    render(){
-        //console.log("this.props.logs[0]: ", this.props.logs[0]);
+    
+    handleKeyPress(event){
+        if(event.key == 'Enter'){
+            console.log('enter press here! ')
+            console.log("Youtr command: "+event.target.value+" can not be executed!");
+            console.log("This feature has been disabled!");
+            // axios.get('http://localhost:5000/command?command='+event.target.value)
+            //     .then(response => {
+            //         console.log(response.data);
+            //         const log = <div>> {event.target.value}<br/>{response.data}<br/></div>
+            //         this.setState({
+            //             list_log: this.state.list_log+log
+            //         });
+            //         event.target.value = "";
+            //     })
+            //     .catch(response => {
+            //         console.log(response);
+            //     });
+        }
+    }
+     
+      render() {
         return (
             <div id="terminal">
                 # =================================================================<br/>
@@ -32,7 +53,17 @@ class dispox_console extends Component {
                 # This console allow you to have logs from the python micro service <br/>
                 # on machine Learning Third party.<br/>
                 # =================================================================<br/><br/>
-                <span>{this.state.list_log}<div>></div></span>
+                <span>{this.state.list_log}
+                    <div>
+                        ><input type="text"
+                                className="invisibleInput"
+                                id="input"
+                                onKeyPress={this.handleKeyPress}/>
+                    </div>
+                    <div style={{ float:"left", clear: "both" }}
+                        ref={(el) => { this.messagesEnd = el; }}>
+                    </div>
+                </span>
             </div>
         );
     }
